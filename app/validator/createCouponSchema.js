5c6698475436ca14557852487432f0c423122265
Pratham Singh <pratham.singh@chcicmic.co.in>
2023-03-21 12:56:51 +0530

    Constants added to validators

--- app/validator/createCouponSchema.js
+++ app/validator/createCouponSchema.js
@@ -4,12 +4,12 @@ const { StringLengthMin, StringLengthMax, } = require('../util/constants');
 const createCouponSchema = Joi.object({
     couponCode: Joi.string().
         min(StringLengthMin).
-        max(StringLengthMax).
+        max(StringLengthMax.Normal).
         uppercase().
         required(),
     couponName: Joi.string().
         min(StringLengthMin).
-        max(StringLengthMax).
+        max(StringLengthMax.Normal).
         required(),
     discountPercentage: Joi.number().min(1).max(100).required(),
     validity: Joi.number().min(1).required(),
